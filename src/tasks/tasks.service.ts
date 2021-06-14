8bcc97031561a508606be0a9661b51d9b78620e2
kiken-eng <kanyecope@gmail.com>
2021-06-14 11:58:21 +0300

    Method to make the tasks array exposed for reading from it

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  //If we make tasks(variable) public, it means other places in our application can accidentally change it yet we don't know
+  private tasks = []; //Array for storage
+
+  getTasks() {
+    // The tasks array is exposed on through the message to read from it
+    return this.tasks;
+  }
+}
