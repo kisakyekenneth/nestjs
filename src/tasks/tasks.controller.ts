a9bb06dd49943321d70a6289b6826ce409a402c4
kiken-eng <kanyecope@gmail.com>
2021-06-14 11:23:07 +0300

    Injected a Service Class

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,9 @@
 import { Controller } from '@nestjs/common';
+import { TasksService } from './tasks.service';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+  //Injecting controller
+  //And having constructor of property taskService of type TaskService
+  constructor(private taskService: TasksService) {}
+}
