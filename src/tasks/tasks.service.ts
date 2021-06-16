700f179cb36d1efe5bd1813dc1a074dfccf44127
kiken-eng <kanyecope@gmail.com>
2021-06-16 13:36:19 +0300

    Added variable of type Task array from our Model

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { Task } from './tasks.model';
 
 @Injectable()
 export class TasksService {
   //If we make tasks(variable) public, it means other places in our application can accidentally change it yet we don't know
-  private tasks = []; //Array for storage
+  private tasks: Task[] = []; //Variable tasks is of datatype Task[] which is exported
 
-  getTasks() {
+  //Method once called, returns an array of Tasks
+  getTasks(): Task[] {
     // The tasks array is exposed on through the message to read from it
     return this.tasks;
   }
