aa304efaf4f32391fbcf61f929e33860ea458879
kiken-eng <kanyecope@gmail.com>
2021-06-17 18:19:43 +0300

    Added POST endpoint, and method to create Tasks

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+
 import { Task } from './tasks.model';
 import { TasksService } from './tasks.service';
 
@@ -11,5 +12,14 @@ export class TasksController {
   getTasks(): Task[] {
     //Since we using model which is of Array type, the required method from service returns an array so this must also return an array.
     return this.taskService.getTasks();
+  }
+
+  @Post()
+  createTask(
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ): Task {
+    //Retrieve the post data using the decorator @Body and store results into body variable
+    return this.taskService.createTask(title, description);
   }
 }
